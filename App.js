2d36a6b37d0251cda7318ef74501f421a9acd763
emre alıcı <emrealici@emre-MacBook-Pro-2.local>
2021-12-19 15:10:52 +0300

    Create theme provider, add icons,paper

--- App.js
+++ App.js
@@ -7,7 +7,9 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+
+import { Provider as PaperProvider,DefaultTheme } from 'react-native-paper';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -18,75 +20,37 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { SoundListScreen } from './src/screen/SoundListScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const darkTheme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#1A1A1A",
+      accent: "#FAFAFA"
+    },
+  };
+  
+  const lightTheme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#FAFAFA",
+      accent: "#1A1A1A",
+    },
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <PaperProvider theme={isDarkMode ? darkTheme : lightTheme}>
+    <SoundListScreen/>
+    </PaperProvider>
   );
 };
 
---
+++ src/screen/SoundListScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+
+const SoundListScreen = () => {
+
+    return (
+        <View >
+            <Text>Hello</Text>
+        </View>
+    );
+};
+
+export { SoundListScreen };
